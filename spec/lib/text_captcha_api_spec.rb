1727837b2434b3f06c00cd042811d23d3f84af6f
Elie Gaboriau <elie@opensourcepolitics.eu>
2022-11-14 17:43:36 +0100

    add spec

--- spec/lib/text_captcha_api_spec.rb
+++ spec/lib/text_captcha_api_spec.rb
@@ -6,22 +6,45 @@ module ActsAsTextcaptcha
   describe TextcaptchaApi do
     subject { described_class.new }
 
-    before do
-      stub_request(:get, "https://textcaptcha.com:80/.json")
-        .with(
-          headers: {
-            "Accept" => "*/*",
-            "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
-            "Host" => "textcaptcha.com",
-            "User-Agent" => "Ruby"
-          }
-        )
-        .to_return(status: 200, body: "Forcing https", headers: {})
+    context "when the API is returning a success code" do
+      before do
+        stub_request(:get, "https://textcaptcha.com:80/.json")
+          .with(
+            headers: {
+              "Accept" => "*/*",
+              "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
+              "Host" => "textcaptcha.com",
+              "User-Agent" => "Ruby"
+            }
+          )
+          .to_return(status: 200, body: "Forcing https", headers: {})
+      end
+
+      describe "#get" do
+        it "returns a hash with the questions" do
+          expect(subject.get).to eq("Forcing https")
+        end
+      end
     end
 
-    describe "#get" do
-      it "returns a hash with the questions" do
-        expect(subject.get).to eq("Forcing https")
+    context "when the API is returing an error code" do
+      before do
+        stub_request(:get, "https://textcaptcha.com:80/.json")
+          .with(
+            headers: {
+              "Accept" => "*/*",
+              "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
+              "Host" => "textcaptcha.com",
+              "User-Agent" => "Ruby"
+            }
+          )
+          .to_return(status: 300, body: "Error", headers: {})
+      end
+
+      describe "#get" do
+        it "returns nothing" do
+          expect(subject.get).to eq(nil)
+        end
       end
     end
   end
